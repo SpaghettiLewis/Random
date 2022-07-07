a49c9c1dd09b53ed951624225177664ed4beba0f
Lewis Aglietti <lewisaglietti@gmail.com>
2022-07-07 23:02:10 +1200

    Magic 8 Ball logo changes

--- magic_8_ball.py
+++ magic_8_ball.py
@@ -1,13 +1,13 @@
 import random
 import time
 
-print('  __  __          _____ _____ _____    ___  ')
-print(' |  \/  |   /\   / ____|_   _/ ____|  / _ \ ')
-print(' | \  / |  /  \ | |  __  | || |      | (_) |')
-print(' | |\/| | / /\ \| | |_ | | || |       > _ < ')
-print(' | |  | |/ ____ \ |__| |_| || |____  | (_) |')
-print(' |_|  |_/_/    \_\_____|_____\_____|  \___/ ')
-print('')
+print("  __  __          _____ _____ _____    ___  ")
+print(" |  \/  |  /\   / ____|_   _/ ____|  / _ \ ")
+print(" | \  / | /  \ | |  __  | || |      | (_) |")
+print(" | |\/| |/ /\ \| | |_ | | || |       > _ < ")
+print(" | |  | | ____ \ |__| |_| || |____  | (_) |")
+print(" |_|  |_//    \_\_____|_____\_____|  \___/ ")
+print("")
 
 possible_answers = ("It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes Signs point to yes", "Reply hazy", "Try again", "Ask again later", "Better not tell you that", "Cannot predict now", "Concentrate and ask again", "Dont count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful")
 user_name = input("Hello player... I am the magic 8 ball... And what might your name be? ")
