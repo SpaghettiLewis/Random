80a4162c1b6a8e0f48f11be6c6dab1bc00b59dd8
Lewis Aglietti <lewisaglietti@gmail.com>
2022-07-07 22:42:55 +1200

    Magic 8 Ball answer generator

--- magic_8_ball.py
+++ magic_8_ball.py
@@ -8,16 +8,9 @@ print(' | |\/| | / /\ \| | |_ | | || |       > _ < ')
 print(' | |  | |/ ____ \ |__| |_| || |____  | (_) |')
 print(' |_|  |_/_/    \_\_____|_____\_____|  \___/ ')
 print('')
-print('')
-print('')
 
-possible_answers = ("It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes Signs point to yes", "Reply hazy", "try again", "Ask again later", "Better not tell you that", "Cannot predict now", "Concentrate and ask again", "Dont count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful")
+possible_answers = ("It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes Signs point to yes", "Reply hazy", "Try again", "Ask again later", "Better not tell you that", "Cannot predict now", "Concentrate and ask again", "Dont count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful")
 user_name = input("Hello player... I am the magic 8 ball... And what might your name be? ")
-
-print("Well hello there {}"
-.format(user_name))
-
-time.sleep(2)
 
 def answer_generator():
     question = input("What is your question for me today? ")
@@ -26,4 +19,13 @@ def answer_generator():
     time.sleep(1)
     print("Very interesting...")
     time.sleep(3)
-    
+    answer_number = random.randint(0, 20)
+    answer = possible_answers[answer_number]
+    print(answer)
+
+print("Well hello there {}"
+.format(user_name))
+
+time.sleep(2)
+
+answer_generator()
